506cd2aa6dd4b69683fb16ef2edc99ee7ecbb077
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-27 12:51:59 -0400

    added .env and configuration for app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require('express');
+const cors = require('cors');
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to Team 2 - Capstone Project')
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('Not Found')
+});
+
+// EXPORT
+module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+// DEPENDENCIES
+const app = require('./app')
 
-const app = express();
-const PORT = 3000;
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, (error) =>{
+// CONNECTION
+app.listen(PORT, (error) => {
 	if(!error)
 		console.log("Server is Successfully Running, and App is listening on port "+ PORT)
 	else
 		console.log("Error occurred, server can't start", error);
 	}
-);
+)
