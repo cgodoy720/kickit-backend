2661f3762e1c287dba7d96ae81c89acc12a56186
Fadila-Ali <fadilaali@pursuit.org>
2023-05-08 10:50:00 -0400

    schema for four tables done

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,47 @@ DROP DATABASE IF EXISTS kickit;
 CREATE DATABASE kickit;
 
 \c kickit;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age INTEGER NOT NULL,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    profile_img TEXT
+);
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    date_created TIMESTAMP WITH TIME ZONE DEFAULT TO_TIMESTAMP(TO_CHAR(CURRENT_TIMESTAMP, 'MM/DD/YYYY'), 'MM?DD?YYYY'),
+    date_event TEXT NOT NULL,
+    category INTEGER[] REFERENCES categories(id) NOT NUll,
+    summary TEXT NOT NULL,
+    max_people INTEGER, 
+    age_restriction BOOLEAN,
+    age_min INTEGER DEFAULT 18,
+    age_max INTEGER,
+    CHECK(age_min <= age_max),
+    location TEXT NOT NULL,
+    creator_id INTEGER REFERENCES user(id)
+    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    events_id INTEGER REFERENCES events(id),
+    user_id INTEGER REFERENCES user(id),
+    comment VARCHAR(150) NOT NULL,
+    time TIMESTAMP WITH TIME ZONE DEFAULT TO_TIMESTAMP(TO_CHAR(CURRENT_TIMESTAMP, 'MM/DD/YYYY'), 'MM?DD?YYYY')
+);
