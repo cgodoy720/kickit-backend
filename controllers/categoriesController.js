f02d65382282b756847c7f6010ac84b6dcc879d7
Fadila-Ali <fadilaali@pursuit.org>
2023-05-09 11:34:46 -0400

    categories controller done

---
+++ controllers/categoriesController.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const categories = express.Router();
+
+const { getAllCategories } = require("../queries/Categories");
+
+//! GET ALL CATEGORIES
+categories.get("/", async (req, res) => {
+    const allCategories = await getAllCategories();
+    if (allCategories[0]) {
+        res.status(200).json(allCategories);
+    } else {
+        res.status(500).json({ error: "server error!"});
+    }
+});
